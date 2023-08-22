7acdfa63fde21409409a1e3a2674f3f63ed87dc3
c0ffeeca7 <k0ffeeka7@gmail.com>
2023-08-22 11:41:11 +0200

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+const yaml = require("js-yaml");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+  eleventyConfig.addPassthroughCopy(
+    {
+      "public/images": "images",
+      "public/resources": "resources",
+      "public/_redirects": "_redirects",
+    }
+  );
+  eleventyConfig.addFilter("sortByOrder", (arr) => {
+    if (!arr) return [];
+    arr.sort((a, b) => (a.data.order > b.data.order ? 1 : -1));
+    return arr;
+  });
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+    htmlTemplateEngine: "liquid",
+  };
+};
